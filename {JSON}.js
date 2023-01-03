e0f6e94122d8dd1b5d81e765f99bd40ba87d12e2
Bonggon Park <devbonggonpark@gmail.com>
2023-01-03 23:46:03 +0900

    Note

---
+++ {JSON}.js
@@ -0,0 +1,30 @@
+//서버통신의 시작
+/*HTTP - Hypertext Transfer Protocal
+    client =>request => sever
+    sever => response => client
+    Hyertext : 전반적으로 쓰고있는 resource 들 (Hyperlink, 문서, 이미지)
+
+    HTTP를 이용해 server에서 data를 요청해 받아올 수 있는 방법
+    - AJAX : Asynchronous JavaScript And XML
+    웹페이지에서 동적으로 server에게 data를 주고받을 수 있는 기술
+
+    대표적인 예:
+    XHR(Object)
+    XMLHttpRequest 
+    간단하게 서버에게 데이터를 요청하고 받아올 수 있음
+
+    XML : 불필요한 태그들이 너무 많이 들어가 파일의 사이즈가 커지고 가독성이 좋지않아 사용이 줄어드는 추세
+    
+    요즘은JSON을 많이 쓰는편
+
+    JSON - JavaScript Object Notation
+    Object {key : value}
+
+    -simplest data interchange format / 데이터를 주고 받을 때 쓸 수  있는 가장 간단한 파일 포멧
+    -lightweight text-based structure / 텍스트를 기반으로 가벼움
+    -easy to read / 사람 눈으로 읽기 편함
+    -key-value pairs / 키와 밸류로 이루어진 파일 포멧
+    -used for serialization and trasmission of data between / 데이터를 보통은 서버와 주고 받을 때 serialization위해 데이터를 전송할 떄 씀
+     the netwrok the network connection
+    -independent programming language and platform / ** 프로그래밍 랭귀지나 플랫폼에 상관없이 사용 가능하다
+*/
